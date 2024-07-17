4bb87311f5907b9f8d6a9ca052d4c5309b21446f
Gekyume <contatogekyume@gmail.com>
2024-07-16 22:13:47 -0300

    switch-ordenacao

--- src/Components/Listagem/Listagem.jsx
+++ src/Components/Listagem/Listagem.jsx
@@ -1,11 +1,44 @@
 import "./Listagem.css";
+import React, { useState, useEffect } from "react";
 import { Produtos } from "../Produtos/Produtos";
 
 export function Listagem({ produtos }) {
-  const produtosOrdenados = [...produtos].sort((a, b) => a.valor - b.valor);
+
+  const [ordenacao, setOrdenacao] = useState("1"); // Estado para ordenação
+
+  // Função para lidar com a mudança na seleção de ordenação
+  const handleOrdenacaoChange = (e) => {
+    setOrdenacao(e.target.value);
+  };
+
+  // Ordena os produtos com base no estado de ordenação
+  const produtosOrdenados = [...produtos].sort((a, b) => {
+    
+    switch (ordenacao) {
+      case "1":
+        return a.valor - b.valor; // Crescente
+      case "2":
+        return b.valor - a.valor; // Decrescente
+      default:
+        return 0; // Nenhuma ordenação aplicada por padrão
+    }
+
+  });
+
+
 
   return (
     <section id="listagem">
+      <select
+        id="ordenacao"
+        className="form-select"
+        aria-label="Default"
+        value={ordenacao}
+        onChange={handleOrdenacaoChange}
+      >
+        <option value="1">Crescente</option>
+        <option value="2">Decrescente</option>
+      </select>
       <table className="table table-dark table-striped table-bordered table-hover">
         <thead>
           <tr>
@@ -28,7 +61,9 @@ export function Listagem({ produtos }) {
         </tbody>
       </table>
 
-      <a href="#formulario" className="btn btn-primary m-5">+</a>
+      <a href="#formulario" className="btn btn-primary m-5">
+        +
+      </a>
     </section>
   );
 }
